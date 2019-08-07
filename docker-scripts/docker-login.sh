c7a16ca6ff58c51d90faf8679688164a71157432
Chris Goldman <dev@oolong.com>
2019-08-07 11:39:37 -0700

    Update region handling

--- docker-scripts/docker-login.sh
+++ docker-scripts/docker-login.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ -z "${AWS_DEFAULT_REGION}" ]]
+if [[ -z "${AWS_REGION}" ]]
 then
-    echo "The AWS_DEFAULT_REGION environment variable must be set to a valid region."
-    exit 1
+    if [[ -z "${AWS_DEFAULT_REGION}" ]]
+    then
+        echo "The AWS_REGION or AWS_DEFAULT_REGION environment variable must be set to a valid region."
+        exit 1
+    fi
+    AWS_REGION="${AWS_DEFAULT_REGION}"
 fi
 
-echo Logging in to Amazon ECR in region: \"${AWS_DEFAULT_REGION}\"
-$(aws ecr get-login --no-include-email --region "${AWS_DEFAULT_REGION}") 2> /dev/null
+echo Logging in to Amazon ECR in region: \"${AWS_REGION}\"
+$(aws ecr get-login --no-include-email --region "${AWS_REGION}") 2> /dev/null
 LOGIN_EXIT_STATUS=$?
 if [[ ${LOGIN_EXIT_STATUS} -ne 0 ]]
 then
